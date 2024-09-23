0c58cd46664cf38a3012c4f572d046a12dcf3a96
MuhammadJareer24 <m.ahmar2496@gmail.com>
2024-09-24 01:33:45 +0500

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import "./index.css"
 
 function App() {
   const [ayahs, setAyahs] = useState([])
-
   const [currentAyah, setCurrentAyah] = useState("")
   const [currentReference, setCurrentReference] = useState("")
-
   const [info, setInfo] = useState(null)
 
   const url =
     "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/eng-muftitaqiusmani.min.json"
-
   const infoUrl =
     "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/info.json"
 
@@ -22,13 +19,11 @@ function App() {
         // Fetch Ayahs
         const ayahsResponse = await fetch(url)
         const ayahsData = await ayahsResponse.json()
-
         setAyahs(ayahsData.quran)
 
         // Fetch Info
         const infoResponse = await fetch(infoUrl)
         const infoData = await infoResponse.json()
-
         setInfo(infoData)
         displayRandomAyah(ayahsData.quran, infoData)
       } catch (error) {
@@ -41,15 +36,11 @@ function App() {
 
   const displayRandomAyah = (ayahsArray, infoData) => {
     const randomIndex = Math.floor(Math.random() * ayahsArray.length)
-
     const randomAyah = ayahsArray[randomIndex]
-
     const chapterInfo = infoData.chapters.find(
       (chapter) => chapter.chapter === randomAyah.chapter
     )
-
     const chapterName = chapterInfo.name
-
     setCurrentAyah(randomAyah.text)
     setCurrentReference(
       `${chapterName} (${randomAyah.chapter}:${randomAyah.verse})`
@@ -60,25 +51,30 @@ function App() {
     displayRandomAyah(ayahs, info)
   }
 
-  const coptToClipboard = () => {
+  const copyToClipboard = () => {
     const copyText = `Ayah: ${currentAyah}\nReference: ${currentReference}`
-
     navigator.clipboard
       .writeText(copyText)
       .then(() => alert("Ayah has been copied"))
       .catch(() => alert("Failed to copy the ayah"))
   }
 
+  const handleWhatsAppShare = () => {
+    const message = `Ayah: ${currentAyah}\nReference: ${currentReference}`
+    const whatsAppUrl = `https://wa.me/?text=${encodeURIComponent(message)}`
+    window.open(whatsAppUrl, "_blank")
+  }
+
   return (
-    <div className="bg-[#f9fafb] font-k2d flex justify-center items-center h-screen m-0 overflow-hidden">
-      <div id="wrapper" className="text-center">
-        <h1 id="title" className="title text-2xl md:text-2xl font-bold pb-5">
+    <div className="bg-[#f9fafb] font-k2d flex justify-center items-center min-h-screen p-5 sm:p-10">
+      <div id="wrapper" className="text-center w-full max-w-[800px]">
+        <h1 id="title" className=" text-3xl md:text-5xl font-bold mb-5">
           Random Quran Ayah
         </h1>
-        <p></p>
+        <p className="mb-8 text-2xl">A source of peace and reflection ❤</p>
         <div
           id="quote-box"
-          className="w-full max-w-md mx-5 p-5 font-semibold text-lg bg-white text-center rounded-lg shadow-lg"
+          className=" mx-5 p-5 font-semibold text-lg bg-white text-center rounded-lg shadow-lg space-y-4"
         >
           <div id="text">
             <blockquote>
@@ -92,21 +88,34 @@ function App() {
               {currentReference}
             </cite>
           </div>
-          <div id="buttons" className="flex justify-between gap-2 mt-5">
+          <div
+            id="buttons"
+            className="flex justify-between  items-center gap-2 mt-5"
+          >
             <button
-              className="newBtn button"
+              className="flex items-center justify-center bg-[#007BFF] text-white rounded-lg px-3 py-2 hover:bg-[#3399ff] transition duration-300 shadow-md"
               id="newBtn"
               onClick={handleNewAyah}
             >
-              <i className="fas fa-sync-alt mx-0.5"></i> New Ayah
+              <i className="fas fa-sync-alt mr-2"></i> New
             </button>
-            <button
-              className="copyBtn button"
-              id="copyBtn"
-              onClick={coptToClipboard}
-            >
-              <i className="fas fa-copy mx-0.5"></i> Copy Ayah
-            </button>
+            {/* Grouping Copy and WhatsApp buttons on the right side */}
+            <div className="flex items-center justify-end space-x-2">
+              <button
+                className="flex items-center justify-center bg-[#17A2B8] text-white rounded-lg px-3 py-3 hover:bg-[#33bbc1] transition duration-300 shadow-md"
+                id="copyBtn"
+                onClick={copyToClipboard}
+              >
+                <i className="fas fa-copy "></i>
+              </button>
+              <button
+                className="flex items-center justify-center bg-[#25D366] text-white rounded-lg px-3 py-2 hover:bg-green-400 transition duration-300 shadow-md"
+                id="whatsappBtn"
+                onClick={handleWhatsAppShare}
+              >
+                <i className="fa-brands fa-whatsapp mr-2"></i> WhatsApp
+              </button>
+            </div>
           </div>
         </div>
       </div>
